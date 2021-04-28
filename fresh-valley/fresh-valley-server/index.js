6855ce8cdadd9f41f6d1fb4a956d2bfcf274603e
ahmedrimon <ahmedrimon@gmail.com>
2021-04-28 10:49:26 +0600

    new item

---
+++ fresh-valley/fresh-valley-server/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+// console.log(process.env.DB_PASS)
+
+const port = process.env.PORT || 5000
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qdobr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    // console.log('connection error', err)
+  const orderCollection = client.db("freshvalley").collection("orders");
+  // perform actions on the collection object
+  console.log('database connected successfully')
+  client.close();
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
